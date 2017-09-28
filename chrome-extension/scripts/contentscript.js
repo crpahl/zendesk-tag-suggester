b34a01644a019e9d18cf25bf2385b82c3b27a421
Clinton Pahl <clinton.p@getjobber.com>
2017-09-28 13:57:20 -0600

    Update javascript to handle multiple tags

--- chrome-extension/scripts/contentscript.js
+++ chrome-extension/scripts/contentscript.js
@@ -2,31 +2,38 @@
 
 (function($){
 	$(document).ready(function(){
+		var suggestions = ['blerg1', 'blerg2', 'blerg3']
+		var tagSuggestions = createSuggestionTags(suggestions);
 
-		var tagSuggestions = createSuggestionTags();
 	    setTimeout(function () {
 	    	var $tags = $('div.ember-view.form_field.tags').get(0)
-	    	console.log($tags)
+	    	//console.log($tags)
     		$(tagSuggestions).insertAfter($tags);
-    		console.log('inserting');
-			console.log(createSuggestionTags());
+    		//console.log('inserting');
+			//console.log(createSuggestionTags());
     	}, 5000);
 	});
 
-	function createSuggestionTags()
+	function createSuggestionTags(tags)
 	{
 		return `<div id="tag-suggestions" class="ember-view form_field" >
 					<label for="">Suggested Tags</label>
 					<div style="border: 1px solid lightgray; padding: 1rem 0.5rem; background-color: white; border-radius: 3px;">
-						${createTag('bleh')}
+						${createTags(tags)}
 					</div>
 				</div>`
 	}
 
-	function createTag(tagName)
+	function createTags(tags)
 	{
-		return `<span style="border: 1px solid gray; padding: 0.5rem; background-color: #49b949; border-radius: 2px; color: black;">
-			${tagName}
-        </span>`
+		var tagString = "";
+
+		tags.forEach(function(tag) {
+			tagString += `<span style="margin-right: 0.5rem; border: 1px solid lightgray; padding: 0.5rem; background-color: #49b949; border-radius: 2px; color: black;">
+				${tag}
+	        </span>`
+  		});
+
+		return tagString
 	}
 })(jQuery);
