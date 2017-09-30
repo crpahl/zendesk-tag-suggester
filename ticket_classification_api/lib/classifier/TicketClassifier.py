a34097a33093e31f1bcca90a9c4328d1e06aa3b8
Clint <crpahl@gmail.com>
2017-09-29 20:08:40 -0700

    Update TicketClassifier.py

--- ticket_classification_api/lib/classifier/TicketClassifier.py
+++ ticket_classification_api/lib/classifier/TicketClassifier.py
@@ -1,18 +1,19 @@
+
 import sys
 import json
-import sys
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.pipeline import Pipeline
 
 def main():
 	communications = sys.argv[0]
 
 	loaded_model = pickle.load(open("model.p", 'rb'))
 
-	x = ['I', "like", "technology"]
-
 	cv = CountVectorizer
 	mult_nb = Pipeline([("count_vectorizer", CountVectorizer(analyzer=lambda x: x)), ("multinomial nb", loaded_model)])
 
-	tags = mult_nb.predict(x)
+	tags = mult_nb.predict(communications)
 
 	json_string = json.dumps(tags)
 	print json_string
