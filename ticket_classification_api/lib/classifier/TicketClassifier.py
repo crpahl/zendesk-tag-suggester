792c9477c44add7ed48248c53e95e27e9e4363b7
Clint <crpahl@gmail.com>
2017-09-29 20:06:27 -0700

    Update TicketClassifier.py

--- ticket_classification_api/lib/classifier/TicketClassifier.py
+++ ticket_classification_api/lib/classifier/TicketClassifier.py
@@ -5,7 +5,15 @@ import sys
 def main():
 	communications = sys.argv[0]
 
-	tags = ['QuickBooks', 'Xero']
+	loaded_model = pickle.load(open("model.p", 'rb'))
+
+	x = ['I', "like", "technology"]
+
+	cv = CountVectorizer
+	mult_nb = Pipeline([("count_vectorizer", CountVectorizer(analyzer=lambda x: x)), ("multinomial nb", loaded_model)])
+
+	tags = mult_nb.predict(x)
+
 	json_string = json.dumps(tags)
 	print json_string
 
